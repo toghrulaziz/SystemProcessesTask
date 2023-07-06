880e23e5461a3cc4743b92c6074cd072474e1495
toghrulaziz <togrul1609@gmail.com>
2023-07-06 23:28:20 +0400

    SystemProcessesTask

---
+++ System.Task.Processes/System.Task.Processes/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Task.Processes.ViewModels;
+using System.Task.Processes.Views;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace System.Task.Processes
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public new MainView? MainWindow { get; set; }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            MainWindow = new MainView
+            {
+                DataContext = new MainViewModel()
+            };
+            MainWindow.Show();
+        }
+
+    }
+}
---
+++ System.Task.Processes/System.Task.Processes/ViewModels/MainViewModel.cs
@@ -0,0 +1,79 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Task.Processes.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+
+        ObservableCollection<Process> processes;
+
+        public ObservableCollection<Process> Processes
+        {
+            get { return processes; }
+            set { Set(ref processes, value); }
+        }
+
+        Process? selectedProcess;
+
+        public Process? SelectedProcess
+        {
+            get { return selectedProcess; }
+            set { Set(ref selectedProcess, value); }
+        }
+
+        string? processToCreate;
+
+        public string? ProcessToCreate
+        {
+            get { return processToCreate; }
+            set { Set(ref processToCreate, value); }
+        }
+
+        public MainViewModel()
+        {
+            Processes = new ObservableCollection<Process>(Process.GetProcesses());
+        }
+
+
+        
+
+        public RelayCommand EndButton
+        {
+            get => new(() =>
+            {
+                var processToRemove = SelectedProcess;
+
+                if (processToRemove != null)
+                {
+                    processToRemove.Kill();
+                    Processes.Remove(processToRemove);
+                }
+            });
+        }
+
+
+
+        public RelayCommand CreateButton
+        {
+            get => new(() =>
+            {
+                if (processToCreate != null)
+                {
+                    var temp = Process.Start(processToCreate);
+
+                    Processes.Add(temp);
+                }
+            });
+        }
+
+
+    }
+}
